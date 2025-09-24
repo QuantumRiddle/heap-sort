20590d148da45d01ad4a4e0e0d96e2deb8ceca7c
QuantumRiddle <148262091+QuantumRiddle@users.noreply.github.com>
2025-09-24 06:53:27 +0300

    have to add counts and movements

--- main.go
+++ main.go
@@ -7,193 +7,99 @@ import (
 
 func main() {
 
-	// a:=generateArr(10)
-	// fmt.Println(a)
-	//a := []int{14, 61, 85, 24, 74, 26, 17, 50, 40, 45, 21, 32, 59, 58, 13}  [16 13 1 13 10 4] [6 1 1 13 6 16] 
-	//						4
-	//				8				2
-	//			2		4		6		3
-	//		9	   2  10 10   9   6   1	  6
-	//
-	a := []int{6, 1, 1, 13, 6, 16}
 
-	// for i := 0; i < 5; i++ {
 
-	// a := generateArr(6)
-	fmt.Println("unsorted ", a)
+	a := generateArr(20)
+	fmt.Println(a)
+	a = swap(a)
+	fmt.Println(a)
 
-	a = createTree(a, len(a)-1)
-	for i := 0; i < len(a)-2; i++ {
-		last := len(a) - 1 - i
-		temp := 0
-		temp = a[0]
-		a[0] = a[last]
-		a[last] = temp
-		last--
-		a = brush(a, last)
+}
+
+func sortTree(arr []int, lenght int) []int {
+
+	var temp int
+
+	for i := lenght / 2; i >= 0; i-- {
+
+		if 2*i+1>lenght {
+			continue
+		}
+
+		if arr[i] <= arr[2*i+1] {
+			temp = arr[i]
+			arr[i] = arr[2*i+1]
+			arr[2*i+1] = temp
+			temp = 0
+			
+		}
+
+		if 2*i+2 > lenght {
+			continue
+		}
+
+		if arr[i] <= arr[2*i+2] {
+			temp = arr[i]
+			arr[i] = arr[2*i+2]
+			arr[2*i+2] = temp
+			temp = 0
+		}
+
 	}
 
-	fmt.Println("sorted: ", a)
+	for i := 0; i < lenght / 2; i++ {
 
-	// }
+		if arr[i] <= arr[2*i+1] {
+			temp = arr[i]
+			arr[i] = arr[2*i+1]
+			arr[2*i+1] = temp
+			temp = 0
+
+		}
+
+		if 2*i+2 > lenght {
+			continue
+		}
+
+		if arr[i] <= arr[2*i+2] {
+			temp = arr[i]
+			arr[i] = arr[2*i+2]
+			arr[2*i+2] = temp
+			temp = 0
+
+		}
+
+	}
+
+	return arr
+
+}
+
+func swap(arr []int) []int {
+
+	var temp int
+	l:=len(arr)-1
+
+	for i := l; i >= 0; i-- {
+		
+		sortTree(arr, l)
+		temp = arr[0]
+		arr[0] = arr[l]
+		arr[l] = temp
+		temp = 0
+		l--
+		
+
+	}
+
+	return arr
 
 }
 
 func generateArr(n int) []int {
-
 	arr := make([]int, n)
-
 	for i := 0; i < n; i++ {
-		arr[i] = rand.Intn(20)
+		arr[i] = rand.Intn(100)
 	}
-
-	return arr
-
-}
-
-func createTree(arr []int, l int) []int {
-
-	var temp int
-
-	lastParrent := l/2 - 1
-
-	for i := lastParrent; i >= 0; i-- {
-
-		temp = 0
-
-		if arr[i] < arr[2*i+1] {
-
-			temp = arr[2*i+1]
-			arr[2*i+1] = arr[i]
-			arr[i] = temp
-			temp = 0
-
-			if arr[i] < arr[2*i+2] {
-				temp = arr[2*i+2]
-				arr[2*i+2] = arr[i]
-				arr[i] = temp
-			}
-			
-		}
-		if arr[i] < arr[2*i+2] {
-			temp = arr[2*i+2]
-			arr[2*i+2] = arr[i]
-			arr[i] = temp
-
-			if arr[i] < arr[2*i+1] {
-				temp = arr[2*i+1]
-				arr[2*i+1] = arr[i]
-				arr[i] = temp
-				
-			}
-			
-		}
-		
-
-	}
-
-	for i := 0; i < lastParrent; i++ {
-		temp = 0
-
-		if arr[i] < arr[2*i+1] {
-
-			temp = arr[2*i+1]
-			arr[2*i+1] = arr[i]
-			arr[i] = temp
-			temp = 0
-			// if i > 1 {
-			// 	i -= 2
-			// }
-
-			if arr[i] < arr[2*i+2] {
-				temp = arr[2*i+2]
-				arr[2*i+2] = arr[i]
-				arr[i] = temp
-				// if i > 1 {
-				// 	i -= 2
-				// }
-				
-			}
-			
-		}
-		if arr[i] < arr[2*i+2] {
-			temp = arr[2*i+2]
-			arr[2*i+2] = arr[i]
-			arr[i] = temp
-			// if i > 1 {
-			// 	i -= 2
-			// }
-			if arr[i] < arr[2*i+1] {
-				temp = arr[2*i+1]
-				arr[2*i+1] = arr[i]
-				arr[i] = temp
-				// if i > 1 {
-				// 	i -= 2
-				// }
-			
-			}
-			
-		}
-		
-	}
-
-	return arr
-
-}
-
-func brush(arr []int, l int) []int {
-
-	var temp int
-
-	lastParrent := l / 2
-
-	for i := 0; i < lastParrent; i++ {
-
-		if arr[i] < arr[2*i+1] {
-
-			temp = arr[2*i+1]
-			arr[2*i+1] = arr[i]
-			arr[i] = temp
-			temp = 0
-			// if i > 1 {
-			// 	i -= 2
-			// }
-
-			if arr[i] < arr[2*i+2] {
-				temp = arr[2*i+2]
-				arr[2*i+2] = arr[i]
-				arr[i] = temp
-				// if i > 1 {
-				// 	i -= 2
-				// }
-		
-			}
-		
-		}
-
-		if arr[i] < arr[2*i+2] {
-			temp = arr[2*i+2]
-			arr[2*i+2] = arr[i]
-			arr[i] = temp
-			// if i > 1 {
-			// 	i -= 2
-			// }
-
-			if arr[i] < arr[2*i+1] {
-				temp = arr[2*i+1]
-				arr[2*i+1] = arr[i]
-				arr[i] = temp
-				// if i > 1 {
-				// 	i -= 2
-				// }
-		
-			}
-		
-		}
-		
-	}
-
-	createTree(arr,l)
-
 	return arr
 }
